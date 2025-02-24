f6a0a49722670fc4dd7ac2ec9792de38458e11bb
st10457625 <st10457625@vcconnect.co.edu>
2025-02-24 21:00:25 +0200

    Added a model

---
+++ PMBVids/Data/PMBVidsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PMBVids.Models;
+
+namespace PMBVids.Data
+{
+    public class PMBVidsContext : DbContext
+    {
+        public PMBVidsContext (DbContextOptions<PMBVidsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PMBVids.Models.Movie> Movie { get; set; } = default!;
+    }
+}
