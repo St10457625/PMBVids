97a40a5e3c9c517f26d8f146be87ab262d66ea37
st10457625 <st10457625@vcconnect.co.edu>
2025-02-24 21:14:35 +0200

    Controller actions and views

--- PMBVids/Models/Movie.cs
+++ PMBVids/Models/Movie.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PMBVids.Models
 {
@@ -6,6 +8,7 @@ namespace PMBVids.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
