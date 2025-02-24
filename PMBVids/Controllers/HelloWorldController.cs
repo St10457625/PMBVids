32413eb2b8e7a27aaad4dbd6ba2a8ae5de91950f
st10457625 <st10457625@vcconnect.co.edu>
2025-02-24 20:12:00 +0200

    Add project files.

---
+++ PMBVids/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMBVids.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
