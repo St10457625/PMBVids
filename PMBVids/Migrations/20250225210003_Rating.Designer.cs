369ab3c0e5e4dcfb19ce7b6b1699539b556b16fc
St10457625 <St10457625@vcconect.edu.za>
2025-02-25 23:11:52 +0200

    Add a new field

--- PMBVids/Migrations/20250225210003_Rating.Designer.cs
+++ PMBVids/Migrations/20250225210003_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PMBVids.Data;
 
@@ -11,9 +12,11 @@ using PMBVids.Data;
 namespace PMBVids.Migrations
 {
     [DbContext(typeof(PMBVidsContext))]
-    partial class PMBVidsContextModelSnapshot : ModelSnapshot
+    [Migration("20250225210003_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
